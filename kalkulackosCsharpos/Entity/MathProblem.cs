42a556bd40762248ccfed02cfa5ca03f1d97d37e
VojtechBaudys <vojta@baudys.name>
2023-03-28 23:10:28 +0200

    super vecicky (asi final)

--- kalkulackosCsharpos/Entity/MathProblem.cs
+++ kalkulackosCsharpos/Entity/MathProblem.cs
@@ -1,27 +1,85 @@
-﻿using System.Data;
+﻿using System;
+using System.Data;
 
 namespace kalkulackosCsharpos.Entity;
 public class MathProblem
 {
 	private string _problem;
-	
+	private string _problemText;
+	private string _prevProblem;
+	private string _prevProblemText;
+
 	public MathProblem()
 	{
 		_problem = "";
+		_problemText = "";
+		_prevProblem = "";
+		_prevProblemText = "";
 	}
+	
+	// getters
 
-	public void AddToText(string character)
+	public string GetProblemText()
 	{
-		_problem += character;
+		return _problemText;
 	}
-
-	public string Evaluate()
+	
+	public string GetPrevProblemText()
+	{
+		return _prevProblemText;
+	}
+	
+	public void AddToProblem(string character, string characterText)
+	{
+		if (CheckLastChar(character))
+		{
+			_problem += character;
+			_problemText += characterText;
+		}
+	}
+	
+	public void Evaluate()
 	{
 		DataTable dt = new DataTable();
-
-		var result = dt.Compute(_problem, "");
-		_problem = result.ToString()!;
+		string result;
 		
-		return _problem;
+		try
+		{
+			result = dt.Compute(_problem, "").ToString()!;
+			_prevProblem = _problem;
+			_prevProblemText = _problemText + "=";
+			_problem = result;
+		}
+		catch
+		{
+			result = "ERROR";
+			_problem = "";
+		}
+		
+		_problemText = result;
+	}
+
+	public void RemoveLastChar()
+	{
+		if (_problem.Length > 0)
+		{
+			_problem = _problem.Remove(_problem.Length - 1, 1);
+			_problemText = _problemText.Remove(_problemText.Length - 1, 1);	
+		}
+	}
+
+	private bool CheckLastChar(string character)
+	{
+		char[] charArray = _problem.ToCharArray();
+		string characters = "+-*/.";
+
+		if (charArray.Length > 0 && characters.Contains(character))
+		{
+			if (characters.Contains(charArray[^1]))
+			{
+				return false;
+			}
+		}
+		return true;
 	}
 }
--- kalkulackosCsharpos/MainWindow.xaml.cs
+++ kalkulackosCsharpos/MainWindow.xaml.cs
@@ -1,44 +1,77 @@
 ﻿using System;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
 using kalkulackosCsharpos.Entity;
+using MahApps.Metro.Controls;
 
 namespace kalkulackosCsharpos
 {
+	// NuGet packages //
+	// MahApps.Metro
+	// WpfAnimatedGif
+	
 	/// <summary>
 	/// Interaction logic for MainWindow.xaml
 	/// </summary>
-	public partial class MainWindow : Window
+	public partial class MainWindow : MetroWindow
 	{
 		private MathProblem _mathProblem;
+		
 		public MainWindow()
 		{
 			InitializeComponent();
 			CreateNewMathProblem();
 		}
-
+		
+		// Math problem init
 		private void CreateNewMathProblem()
 		{
 			_mathProblem = new MathProblem();
-			MyDisplay.Text = "";
+			RenderProblem();
+		}
+		
+		// Render display
+		private void RenderProblem()
+		{
+			MyDisplay.Text = _mathProblem.GetProblemText();
+			PrevProblem.Text = "";
 		}
 
+		private void RenderPrevProblem()
+		{
+			PrevProblem.Text = _mathProblem.GetPrevProblemText();
+		}
+
+		// EVENTS //
+		
+		// Base buttons click
 		private void ButtonBase(object sender, RoutedEventArgs e)
 		{
 			Button element = (Button)sender;
-			MyDisplay.Text += (string)element.Content;
-			_mathProblem.AddToText((string)element.Tag);
+			_mathProblem.AddToProblem((string)element.Tag, (string)element.Content);
+			RenderProblem();
 		}
 		
+		// Evaluate button click
 		private void ButtonEvaluate(object sender, RoutedEventArgs e)
 		{
-			string result = _mathProblem.Evaluate();
-			MyDisplay.Text = result;
+			_mathProblem.Evaluate();
+			RenderProblem();
+			RenderPrevProblem();
 		}
 
+		// New math problem button click
 		private void ButtonNewMathProblem(object sender, RoutedEventArgs e)
 		{
 			CreateNewMathProblem();
+		}
+
+		// Remove last char button click
+		void ButtonRemoveLast(object sender, RoutedEventArgs e)
+		{
+			_mathProblem.RemoveLastChar();
+			RenderProblem();
 		}
 	}
 }
